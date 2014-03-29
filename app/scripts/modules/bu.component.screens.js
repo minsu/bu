23380667de2560841d8c6efc36964de90304e088
Minsu Kang <minsu@netsco.kr>
2014-03-29 17:16:08 +0900

    update

--- app/scripts/modules/bu.component.screens.js
+++ app/scripts/modules/bu.component.screens.js
@@ -2,9 +2,9 @@
 // bu: screens (directive)
 //-------------------------------------------------------------------
 angular.module('bu').directive('buScreens', [
-  '$log', '$timeout',
+  '$log', '$timeout', 'bu.$service',
 
-  function($log, $timeout) {
+  function($log, $timeout, $bu) {
     return function(scope, element, attrs) {
       $timeout(function() {
         $bu.fire('bu.screens', 'BU_EVENT_DOMREADY');
